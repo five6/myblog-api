84b325eafdfedb2d4a5426545af15fce03c49cf5
zhangxiangzhong <936333511@qq.com>
2020-01-22 10:17:14 +0800

    first commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('小程序后台接口')
+    .setDescription('小程序后台接口描述')
+    .setVersion('1.0')
+    // .addTag('user')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3000);
+}
+bootstrap();
